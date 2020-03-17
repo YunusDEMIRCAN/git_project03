57da187ee5a593c078bda98dcfbda123df11d412
YunusDemircan <yunusdemircan@gmail.com>
2020-03-16 20:40:58 -0400

    next commit local master

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -5,6 +5,8 @@ public class GitDay4 {
 	public static void main(String[] args) {
 		System.out.println("local first commit master branch");
 		System.out.println("local branch first commit");
+		System.out.println("Someone else contributed to the code");
+		System.out.println("my next commit from local master");
 
 	}
 
