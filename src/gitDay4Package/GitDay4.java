3a97a870547ea4440947ea77c2c01f3232f95249
YunusDemircan <yunusdemircan@gmail.com>
2020-03-16 20:31:52 -0400

    local first commit master branch

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -3,7 +3,8 @@ package gitDay4Package;
 public class GitDay4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("local first commit master branch");
+		
 
 	}
 
